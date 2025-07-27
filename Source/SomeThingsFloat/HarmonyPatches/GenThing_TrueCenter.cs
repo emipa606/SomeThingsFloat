53141dbb00a205c41999d005030ec341c4504696
Mlie <emipa606@users.noreply.github.com>
2025-07-27 18:23:25 +0200

    Alive pawns should now also have smooth animation when floating

--- Source/SomeThingsFloat/HarmonyPatches/GenThing_TrueCenter.cs
+++ Source/SomeThingsFloat/HarmonyPatches/GenThing_TrueCenter.cs
@@ -28,3 +28,27 @@ public static class GenThing_TrueCenter
         __result = component.GetNewCenter(t, __result);
     }
 }
+
+[HarmonyPatch(typeof(Pawn_DrawTracker), nameof(Pawn_DrawTracker.DrawPos), MethodType.Getter)]
+public static class Pawn_DrawTracker_DrawPos
+{
+    public static void Postfix(Pawn ___pawn, ref Vector3 __result)
+    {
+        if (!SomeThingsFloatMod.Instance.Settings.SmoothAnimation)
+        {
+            return;
+        }
+
+        if (___pawn?.Map == null)
+        {
+            return;
+        }
+
+        if (!SomeThingsFloat.FloatingMapComponents.TryGetValue(___pawn.Map, out var component))
+        {
+            return;
+        }
+
+        __result = component.GetNewCenter(___pawn, __result);
+    }
+}
